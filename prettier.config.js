33c0a66ec4c9993cf8851ac3a541d55fa6c0d56b
Webb Lu <tsung.ying.lu@gmail.com>
2018-01-04 01:04:51 +0800

    update eslint for jsx always show error when import react

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,7 @@ module.exports = {
   useTabs: false,
   semi: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
   bracketSpacing: true,
   jsxBracketSameLine: true,
   arrowParens: 'always',
