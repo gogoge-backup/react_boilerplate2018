942f01e710b5f2042a3ff8ccce9455378dcd760c
Webb Lu <tsung.ying.lu@gmail.com>
2018-01-05 17:18:44 +0800

    add react es6 class to support arrow event handler

--- src/App/Containers/Main/Main.js
+++ src/App/Containers/Main/Main.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
 import styles from './main.css'
-const Main = () => (
-  <div>
-    <h1>{'Main'}</h1>
-  </div>
-)
-// export default Main
-export default CSSModules(Main, styles)
+
+@CSSModules(styles)
+export default class Main extends Component {
+  componentWillMount() {
+    this.setState({
+      k: 3,
+    })
+  }
+
+  state = {
+    k: 2,
+  }
+
+  click = () => {
+    console.warn(this.state)
+  }
+  render() {
+    return (
+      <div styleName="tg">
+        <h1 styleName="tt">{'Main'}</h1>
+        <button onClick={this.click}>Go</button>
+      </div>
+    )
+  }
+}
+
+// export default CSSModules(Main, styles)
