9c6c026a40b8b40b1d9d1c8f26253f6ae9c45ae2
Webb Lu <tsung.ying.lu@gmail.com>
2018-01-05 11:00:50 +0800

    add extract-text-webpack-plugin, extract css to separate css file

--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -7,6 +7,29 @@ const path = require('path')
 module.exports = merge(common, {
   entry: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000'],
   // devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader?sourceMap',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true, // default is false
+              sourceMap: true,
+              importLoaders: 1,
+              // alias: {
+              //  'MAIN':'src/App/Containers/Main'
+              // },
+              localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+            },
+          },
+        ],
+      },
+    ],
+  },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
--- configs/webpack.prod.js
+++ configs/webpack.prod.js
@@ -2,11 +2,37 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 const PUBLIC_FOLDER_PATH = path.resolve('build')
 
 module.exports = merge(common, {
   // devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader?sourceMap',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true, // default is false
+                sourceMap: true,
+                importLoaders: 1,
+                // alias: {
+                //  'MAIN':'src/App/Containers/Main'
+                // },
+                localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+              },
+            },
+          ],
+        }),
+      },
+    ],
+  },
   plugins: [
     new webpack.SourceMapDevToolPlugin({
       // source map
@@ -21,6 +47,11 @@ module.exports = merge(common, {
       filename: 'index.html',
       inject: 'body', // auto inject the bundle files
       template: path.resolve('template.html'),
+    }),
+    new ExtractTextPlugin({
+      filename: 'css/[name].css?[hash]-[chunkhash]-[contenthash]',
+      disable: false,
+      allChunks: true,
     }),
   ],
 })
